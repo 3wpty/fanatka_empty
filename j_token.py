ed73626a17867f6acd54f0aede4ab7018e4fcbbc
3wpty <trainmakta@gmail.com>
2023-04-28 22:57:19 +0300

    Глобально обновил бота
    Удалил лишние токены

--- j_token.py
+++ j_token.py
@@ -1,23 +1,3 @@
-
-def jett_TOKEN():  #jett
-    return "NzU4MDI4NTIwNjE3Mjc5NTE4.X2o_Eg.D1YxsKlURfaxQ-je8skY9OJ7kwc"
-    
-def sage_TOKEN():  #sage
-    return "ODY2OTg4NDc5NTEwMDg1NjQz.YPaj8Q.7Rw7JY7tjRN0cIcwYwtUnDV0Ie4"
-
-def feddy_TOKEN():  #feddy
-    return "OTYxMzUwODgxMTgwMjU4MzQ0.Yk3txw.BuY6t39Zjftm6-aDbFEq7A0uBr8"
-
-def girl_TOKEN():  #j3tt from 3wory#6113
+def girl_TOKEN():  #bot from 3wory#6113
     return "MTA5OTMxMDc5MjQ1ODg5NTQ5Mw.GBOF_r.MlKt4SWoPDT2S9eX6SIQUCiGhVnDxrSnpDP9QA"
-
-def neon_TOKEN():  #neon for TvisT
-    return "MTA5OTY3NjM0MjIzODMzNTA5Nw.G1lEiR.rnZGQBMCpS_MSAHqr9KojchGlu7CWkkrfXlU0A"
-
-
-
-#       await ctx.message.add_reaction(emoji="❌")  # add reaction
-#       await ctx.message.add_reaction(emoji="✅")  # add reaction
-
-
 
